cba248e27746eea8ae110c77294af290301c6f62
Mário Martins Aragão <marioestudos.nuvem@gmail.com>
2025-01-14 23:54:04 -0300

    Create main.py

---
+++ sem solid/main.py
@@ -0,0 +1,127 @@
+from Aluno import Aluno
+from Professor import Professor
+import os
+import platform
+
+aluno = Aluno()
+professor = Professor()
+alunos = []
+professores = []
+disciplinas = []
+cursos = []
+
+def limparTela():
+    if platform.system() == "Windows":
+        os.system('cls')  # Para Windows
+    else:
+        os.system('clear')  # Para Linux/macOS
+
+def menu():
+    print("1 - Gerenciar Aluno")
+    print("2 - Gerenciar Professor")
+    print("3 - Gerenciar Disciplinas")
+    print("4 - Gerenciar Cursos")
+    print("5 - Sair")
+
+def menuAluno():
+    print("1 - Adicionar Aluno")
+    print("2 - Editar Aluno")
+    print("3 - Ver Alunos")
+    print("4 - Excluir Aluno")
+    print("5 - Voltar ao menu Principal")
+    
+def menuProfessor():
+    print("1 - Adicionar Professor")
+    print("2 - Editar Professor")
+    print("3 - Ver Professors")
+    print("4 - Excluir Professor")
+    print("5 - Voltar ao menu Principal")
+    
+def menuDisciplina():
+    print("1 - Adicionar Disciplina")
+    print("2 - Editar Disciplina")
+    print("3 - Ver Disciplinas")
+    print("4 - Excluir Disciplina")
+    print("5 - Voltar ao menu Principal")
+    
+def escolhasAluno(escolhaAluno):
+    if escolhaAluno == 1:
+        limparTela()
+        aluno.criarAluno(alunos)
+        
+    elif escolhaAluno == 2:
+        limparTela()
+        id = int(input("Digite o ID do aluno: "))
+        aluno.editarAluno(id, alunos)
+        
+    elif escolhaAluno == 3:
+        limparTela()
+        aluno.verAlunos(alunos)
+        
+    elif escolhaAluno == 4:
+        limparTela()
+        id = int(input("Digite o ID do aluno: "))
+        aluno.excluirAluno(id, alunos)
+    
+    elif escolhaAluno == 5:
+        return False
+    
+    return True
+
+
+def escolhasProfessor(escolhaProfessor):
+    if escolhaProfessor == 1:
+        limparTela()
+        professor.criarProfessor(professores)
+        
+    elif escolhaProfessor == 2:
+        limparTela()
+        id = int(input("Digite o ID do professor: "))
+        professor.editarProfessor(id, professores)
+        
+    elif escolhaProfessor == 3:
+        limparTela()
+        professor.verProfessores(professores)
+        
+    elif escolhaProfessor == 4:
+        limparTela()
+        id = int(input("Digite o ID do professor: "))
+        professor.excluirProfessor(id, professores)
+    
+    elif escolhaProfessor == 5:
+        return False
+    
+    return True
+
+    
+def escolhasDisciplina(escolhaDisciplina):
+    if escolhaDisciplina == 1:
+        pass
+    elif escolhaDisciplina == 2:
+       pass
+    elif escolhaDisciplina == 5:
+        return False
+    
+
+
+
+while(True):
+    menu()
+    escolha = int(input("Digite sua escolha: "))
+    if escolha == 1:
+        while(True):
+            menuAluno()
+            escolhaAluno = int(input("Digite sua escolha: "))
+            if not escolhasAluno(escolhaAluno):
+                limparTela()
+                break
+    if escolha == 2:
+        while(True):
+            menuProfessor()
+            escolhaProfessor = int(input("Digite sua escolha: "))
+            if not escolhasProfessor(escolhaProfessor):
+                limparTela()
+                break
+    elif escolha == 5:
+        limparTela()
+        break
