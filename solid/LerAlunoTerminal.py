afd5efa8be021f7006eece5786585b65a1e3d22b
Mário Martins Aragão <marioestudos.nuvem@gmail.com>
2025-01-15 23:06:35 -0300

    Update LerAlunoTerminal.py

--- solid/LerAlunoTerminal.py
+++ solid/LerAlunoTerminal.py
@@ -1,11 +1,11 @@
 class LerAlunoTerminal:
-    def tryCast(value, type):
+    def tryCast(self, value, type):
         try:
             return type(value)
         except:
             return False
 
-    def lerDoTerminal(self,type, prompt):
+    def lerDoTerminal(self, type, prompt):
         input_value = input(prompt)
         while not self.tryCast(input_value, type):
             input_value = input(prompt)
@@ -15,5 +15,5 @@ class LerAlunoTerminal:
         id = self.lerDoTerminal(int,"Digite o id do aluno: ")
         nome = self.lerDoTerminal(str,"Digite o nome do aluno: ")
         idade = self.lerDoTerminal(int,"Digite a idade do aluno: ")
-        curso = self.lerDoTerminal(str,"Digite o curso do aluno")
+        curso = self.lerDoTerminal(str,"Digite o curso do aluno: ")
         return id,nome,idade,curso
